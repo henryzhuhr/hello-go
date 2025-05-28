79d6aae3aed2becedb2f54867a307e8ec3de43ac
henryzhuhr <henry.zhuhr@icloud.com>
2025-05-28 09:43:08 +0800

    🐳 build(Dockerfile): 重构 Golang 环境安装步骤
    - 移除 Golang 环境安装的执行代码
    - 重新安排 apt 缓存清理逻辑
    - 调整 ARG 和 ENV 变量的定义顺序
    - 注释掉 Golang 环境安装步骤，留待后续处理

---
+++ scripts/init.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Install Go tools
+export GOPROXY=https://goproxy.cn,direct
+go env -w GO111MODULE=on
+go env -w GOPROXY=https://goproxy.cn,direct
+go install golang.org/x/tools/gopls@latest
+go install github.com/cweill/gotests/...@latest
+go install github.com/fatih/gomodifytags@latest
+go install github.com/josharian/impl@latest
+go install github.com/haya14busa/goplay/cmd/goplay@latest
+go install github.com/go-delve/delve/cmd/dlv@latest
+go install github.com/golangci/golangci-lint/cmd/golangci-lint@latest
+
+go mod tidy
